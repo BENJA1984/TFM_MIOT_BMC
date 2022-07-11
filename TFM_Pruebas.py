a9dee760d735350b4d5c0642236d135dc760d9fc
BENJA1984 <benja_fr20@hotmail.com>
2022-07-11 18:47:35 +0200

    Descripción Dataset

--- TFM_Pruebas.py
+++ TFM_Pruebas.py
@@ -1,3 +1,7 @@
 
 import pandasql as ps
 import pandas as pd
+DatosPrueba = pd.read_csv(r'C:\Users\benja\Downloads\full_data.csv')
+print(DatosPrueba.head(2))
+describe = DatosPrueba.describe()
+print(describe)
