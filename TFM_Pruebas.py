f4f25708aa1ce0fe22a6ade42ec9c01c961077d6
BENJA1984 <benja_fr20@hotmail.com>
2022-07-12 18:16:07 +0200

    12_07_2022_V1

--- TFM_Pruebas.py
+++ TFM_Pruebas.py
@@ -5,3 +5,7 @@ DatosPrueba = pd.read_csv(r'C:\Users\benja\Downloads\full_data.csv')
 print(DatosPrueba.head(2))
 describe = DatosPrueba.describe()
 print(describe)
+
+import zipfile
+with zipfile.ZipFile(r'C:\Users\benja\Downloads\ashrae-energy-prediction.zip', 'r') as zip_ref:
+    zip_ref.extractall(r'C:\Users\benja\Downloads\data')
