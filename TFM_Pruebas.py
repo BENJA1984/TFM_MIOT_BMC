d6ec03bad16f9b7eb583ad3a505803c854c7429c
BENJA1984 <benja_fr20@hotmail.com>
2022-07-16 10:47:08 +0200

    Prueba1

--- TFM_Pruebas.py
+++ TFM_Pruebas.py
@@ -9,3 +9,4 @@ print(describe)
 import zipfile
 with zipfile.ZipFile(r'C:\Users\benja\Downloads\ashrae-energy-prediction.zip', 'r') as zip_ref:
     zip_ref.extractall(r'C:\Users\benja\Downloads\data')
+print("a")
